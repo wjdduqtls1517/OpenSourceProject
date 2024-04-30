2623bc6d20432f8cd168743758b7bec33ffc08df
jon1517 <jeongyeup8306@gmail.com>
2024-04-30 22:51:58 +0900

    창 띄우기 기능

---
+++ window.py
@@ -0,0 +1,34 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QDesktopWidget
+from PyQt5.QtGui import QFont
+
+
+class MyApp(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('My First Application')
+        self.resize(700, 500)
+
+        # 창을 화면의 정가운데에 위치시키기
+        self.center()
+
+
+
+    def center(self):
+        # 화면의 가운데 위치 구하기
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyApp()
+    sys.exit(app.exec_())
