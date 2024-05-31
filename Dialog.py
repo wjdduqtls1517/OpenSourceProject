15378e7cf84ac29b59771e22ba5531c0b4327014
GoSaeMin <rhtpals13@naver.com>
2024-05-31 21:00:12 +0900

    knowhow

---
+++ Dialog.py
@@ -0,0 +1,22 @@
+
+import sys
+from PyQt5.QtWidgets import (QApplication, QWidget, QLabel, QLineEdit, QPushButton,
+                             QVBoxLayout, QHBoxLayout, QDesktopWidget, QTextEdit, QDialog)
+from PyQt5.QtGui import QFont, QScreen
+from PyQt5.QtCore import Qt
+
+class InputDialog(QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle('텍스트 입력')
+        self.text_edit = QTextEdit()
+        layout = QVBoxLayout()
+        layout.addWidget(self.text_edit)
+        confirm_button = QPushButton('확인')
+        confirm_button.clicked.connect(self.accept)
+        layout.addWidget(confirm_button)
+        self.setLayout(layout)
+ if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = QuestionWindow()
+    sys.exit(app.exec_())
