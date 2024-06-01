b72b599d21ee80be784c23ac079d3928328817e5
jon1517 <jeongyeup8306@gmail.com>
2024-06-02 00:43:12 +0900

    세 번째 피이지 구현

--- ui.py
+++ ui.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QHBoxLayout, QRadioButton, QComboBox, QCheckBox, \
-    QGridLayout, QPushButton, QDesktopWidget, QStackedWidget
+    QGridLayout, QPushButton, QDesktopWidget, QStackedWidget, QFrame, QTextEdit
 from PyQt5.QtGui import QFont
 from PyQt5.QtCore import Qt
 
@@ -19,12 +19,15 @@ class MyApp(QWidget):
         self.stack = QStackedWidget(self)
         self.first_page = QWidget()
         self.second_page = QWidget()
+        self.third_page = QWidget()
 
         self.initFirstPage()
         self.initSecondPage()
+        self.initThirdPage()
 
         self.stack.addWidget(self.first_page)
         self.stack.addWidget(self.second_page)
+        self.stack.addWidget(self.third_page)
 
         main_layout = QVBoxLayout()
         main_layout.addWidget(self.stack)
@@ -92,13 +95,13 @@ class MyApp(QWidget):
 
         # 질병 체크박스 레이아웃
         disease_layout = QGridLayout()
-        diseases = [
+        self.diseases = [
             '간질환', '갑상선', '고혈압', '골다공증', '관절염', '노인성빈혈',
             '노인성우울증', '녹내장', '뇌동맥류', '뇌졸증', '당뇨병', '동맥경화증',
             '변비', '파킨슨병', '신장병', '오십견', '요통', '위장병', '치매',
             '통풍', '퇴행성근골격장애'
         ]
-        disease_info = [
+        self.disease_info = [
             '간질환 정보', '갑상선 정보', '고혈압 정보', '골다공증 정보', '관절염 정보',
             '노인성빈혈 정보', '노인성우울증 정보', '녹내장 정보', '뇌동맥류 정보', '뇌졸증 정보',
             '당뇨병 정보', '동맥경화증 정보', '변비 정보', '파킨슨병 정보', '신장병 정보',
@@ -106,13 +109,15 @@ class MyApp(QWidget):
             '퇴행성근골격장애 정보'
         ]
 
-        for i, disease in enumerate(diseases):
+        self.disease_checkboxes = []
+        for i, disease in enumerate(self.diseases):
             checkbox = QCheckBox(disease, self)
             checkbox.setFont(QFont('Noto Sans', 14))
-            checkbox.setToolTip(disease_info[i])
+            checkbox.setToolTip(self.disease_info[i])
             row = i // 7
             col = i % 7
             disease_layout.addWidget(checkbox, row, col)
+            self.disease_checkboxes.append(checkbox)
         vbox.addLayout(disease_layout)
 
         # 버튼 레이아웃
@@ -121,7 +126,12 @@ class MyApp(QWidget):
         prev_button.setFont(QFont('Noto Sans', 14))
         prev_button.setFixedSize(100, 40)
         prev_button.clicked.connect(self.prevPage)
+        next_button = QPushButton('다음', self)
+        next_button.setFont(QFont('Noto Sans', 14))
+        next_button.setFixedSize(100, 40)
+        next_button.clicked.connect(self.nextPage)
         button_layout.addWidget(prev_button, alignment=Qt.AlignLeft)
+        button_layout.addWidget(next_button, alignment=Qt.AlignRight)
         vbox.addLayout(button_layout)
 
         self.second_page.setLayout(vbox)
@@ -177,8 +187,8 @@ class MyApp(QWidget):
         caution_disease_frame.setFrameShape(QFrame.Box)
         vbox.addWidget(caution_disease_frame)
 
-        # 알아보기 버튼
-        learn_more_caution_button = QPushButton('알아보기', self)
+        # 더 알아보기 버튼
+        learn_more_caution_button = QPushButton('더 알아보기', self)
         learn_more_caution_button.setFont(QFont('Noto Sans', 14))
         learn_more_caution_button.setFixedSize(120, 40)
         caution_disease_box.addWidget(learn_more_caution_button, alignment=Qt.AlignRight)
@@ -240,8 +250,9 @@ class MyApp(QWidget):
 
         self.caution_disease_list.setText('\n'.join(caution_diseases))
 
-        if __name__ == '__main__':
-            app = QApplication(sys.argv)
-            ex = MyApp()
-            ex.show()
-            sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyApp()
+    ex.show()
+    sys.exit(app.exec_())
