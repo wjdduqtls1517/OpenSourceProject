787abc942b8dd8576833a7c7fda5bea84d4c0678
DESKTOP-N6EVNIS\user <asdfghdebdo@gmail.com>
2024-06-07 12:56:52 +0900

    질병 레이아웃 관련3

--- ui.py
+++ ui.py
@@ -286,6 +286,11 @@ class MyApp(QWidget):
         scroll_area.setWidgetResizable(True)
         scroll_content = QWidget()
         scroll_content.setLayout(self.disease_buttons_layout)
+        scroll_area.setWidget(scroll_content)
+        vbox.addWidget(scroll_area)
+
+        # 이전 버튼
+        button_layout = QHBoxLayout()
 
     def nextPage(self):
         current_index = self.stack.currentIndex()
