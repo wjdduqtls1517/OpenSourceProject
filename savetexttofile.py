e3337485773c3843d5e08cd6ed27daef86e540cc
GoSaeMin <rhtpals13@naver.com>
2024-06-08 06:02:01 +0900

    savetexttofile4

---
+++ savetexttofile.py
@@ -0,0 +1,5 @@
+ def save_text_to_file(self, selected_disease, name, text):
+        file_name = f'{selected_disease.replace(" ", "_")}_노하우.txt'
+        with open(file_name, 'a', encoding='utf-8') as file:
+            file.write(f"{name}: {text}\n")
+
